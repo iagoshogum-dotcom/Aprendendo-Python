9662a9af555a9aae2b1400769f7ec1d6c31ec08e
iagoshogum-dotcom <iagoshogum@gmail.com>
2026-05-20 17:31:24 -0300

    coisas

---
+++ exercicio12.py
@@ -0,0 +1,30 @@
+exer1 = (15>7) and (3==3)
+print(f"a resposta é {exer1} ")
+
+exer2 = (10<4) or (8 != 5)
+print(f"a resposta é {exer2}")
+
+exer3 =not (20 >= 20)
+print(f"a resposta é {exer3}")
+
+exer4 = (5 * 2 == 10) and not (3 > 4)
+print(f"a resposta é {exer4}")
+
+exer5 = (12 / 2 ==5) or (7 <= 7)
+
+print(f"a resposta é {exer5}")
+
+exer6 = not (9 != 9) and (4 > 1)
+print(f"a resposta é {exer6}")
+
+exer7 = (100 == 100) and ((5 > 10) or (2 < 3))
+print(f"a resposta é {exer7}")
+
+exer8 = not ((18 < 20) and ( 5 != 5))
+print(f"a resposta é {exer8}")
+
+exer9 = ( 0 == 1) or not (( 3 >= 2) and (2 < 3))
+print(f"a resposta é {exer9}")
+
+exer10 = ((10 > 5) and not (2 ==3)) or (8 < 4)
+print(f"a resposta é {exer10}")
